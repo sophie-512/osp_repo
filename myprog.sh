2dfeb9a6cae721ed6f1b2bea4be7d5f24a026c13
sophie-512 <70555100+sophie-512@users.noreply.github.com>
2022-03-30 22:32:18 +0900

    Update myprog.sh

--- myprog.sh
+++ myprog.sh
@@ -1,17 +1,13 @@
 #!/bin/bash
 mkdir temp
 echo "...create temp directory..."
-mv num1.txt temp/num1.txt
-mv num2.txt temp/num2.txt
-mv cal.sh temp/cal.sh
+cp num1.txt num2.txt cal.sh temp/
 echo "...copy files to temp directory..."
-echo "1) add"
-echo "2) sub"
-echo "3) div"
-echo "4) mul"
-read -p "select menu : " parameter
-echo "...add selected..."
+PS3='select menu : '
+select parameter in "add" "sub" "div" "mul"
+do
+  echo "...$parameter selected..."
+  break  
+done
 echo "...run calculater..."
-
-cd temp
 ./cal.sh $parameter
